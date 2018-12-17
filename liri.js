fa53dcb6080b581bf574de79bb9d8403d549800a
World Wide <agwalker249@gmail.com>
2018-12-17 15:23:24 -0600

    obtained bandsintown api key - created working concert-this

--- liri.js
+++ liri.js
@@ -3,6 +3,7 @@ var Spotify = require('node-spotify-api');
 var request = require("request");
 var chalk = require("chalk");
 var fs = require("fs");
+var moment = require("moment");
 
 dotenv.config();
 
@@ -14,42 +15,42 @@ var command = process.argv[2]
 
 
 var content = process.argv[3];
-for(var i = 4; i < process.argv.length; i++){
-    content += "+" + process.argv[i];
+for (var i = 4; i < process.argv.length; i++) {
+    content += " " + process.argv[i];
 }
 
-switch (command){
+switch (command) {
     case "movie-this":
-    movieThis();
-    break;
+        movieThis();
+        break;
 
     case "concert-this":
-    concertThis();
-    break;
+        concertThis();
+        break;
 
     case "spotify-this-song":
-    spotifyThis();
-    break
+        spotifyThis();
+        break
 
     case "do-what-it-says":
-    doWhatItSays();
-    break;
+        doWhatItSays();
+        break;
 
     default:
-    console.log("I don't know how to do that");
-    break;
+        console.log("I don't know how to do that");
+        break;
 
 
 }
 
-function movieThis(){
+function movieThis() {
 
     var queryUrl = "http://www.omdbapi.com/?t=" + content + "&y=&plot=short&apikey=trilogy";
 
     console.log(queryUrl);
 
-    request(queryUrl, function(err, response, body){
-        if(!err && response.statusCode === 200){
+    request(queryUrl, function (err, response, body) {
+        if (!err && response.statusCode === 200) {
 
             console.log(chalk.black.bgCyan("Title") + ' ' + JSON.parse(body).Title);
             console.log(chalk.black.bgCyan("Year released:") + ' ' + JSON.parse(body).Year);
@@ -65,78 +66,96 @@ function movieThis(){
 }
 
 
-//********** Need an api key **********  */
-function concertThis(){
+
+function concertThis() {
 
 
-    var queryUrl = "https://rest.bandsintown.com/artists/" + content + "/events?app_id=*NEEDKEY*"
+    var queryUrl = "https://rest.bandsintown.com/artists/" + content + "/events?app_id=8fb65637e592547afe5889341ee4cf88"
 
-    console.log(queryUrl);
-    
-    request(queryUrl, function(err, response, body){
-        if(!err && response.statusCode === 200){
+    request(queryUrl, function (err, response, body) {
+        if (!err && response.statusCode === 200) {
             debugger;
 
             var jsonData = JSON.parse(body);
 
+            if (!jsonData.length) {
+                console.log("No results found for " + content);
+                return;
+            }
 
-            console.log(jsonData.venue);
-            console.log(jsonData.address);
-            console.log(jsonData.date);
-            
+            console.log("Upcoming concerts for " + content + ":");
+
+            for (var i = 0; i < jsonData.length; i++) {
+
+                var show = jsonData[i];
+
+                console.log(
+                    show.venue.city +
+                    "," +
+                    (show.venue.region || show.venue.country) +
+                    " at " +
+                    show.venue.name +
+                    " " +
+                    moment(show.datetime).format("MM/DD/YYYY")
+
+                );
+
+
+            }
+
         }
     })
 
 }
 
 
-function spotifyThis(){
+function spotifyThis() {
 
-    spotify.search({ type: 'track', limit: '3', query: content }, function(err, data) {
+    spotify.search({ type: 'track', limit: '3', query: content }, function (err, data) {
         if (err) {
-          return console.log('Error occurred: ' + err);
+            return console.log('Error occurred: ' + err);
 
-          
+
         }
 
         var songs = data.tracks.items;
 
         for (var i = 0; i < songs.length; i++) {
-          
-          console.log("\n")
-          console.log(chalk.black.bgCyan("Artist:") + " " + songs[i].album.artists[0].name);
-          console.log(chalk.black.bgCyan("Song name:") + " " + " "  + songs[i].name);
-          console.log(chalk.black.bgCyan("Preview song:") + " " + songs[i].preview_url);
-          console.log(chalk.black.bgCyan("Album:") + " " + songs[i].album.name);     
-          console.log("-----------------------------------");
+
+            console.log("\n")
+            console.log(chalk.black.bgCyan("Artist:") + " " + songs[i].album.artists[0].name);
+            console.log(chalk.black.bgCyan("Song name:") + " " + " " + songs[i].name);
+            console.log(chalk.black.bgCyan("Preview song:") + " " + songs[i].preview_url);
+            console.log(chalk.black.bgCyan("Album:") + " " + songs[i].album.name);
+            console.log("-----------------------------------");
         }
 
-        
 
-   
-    
-      });
+
+
+
+    });
 
 }
 
-function doWhatItSays(){
+function doWhatItSays() {
 
-    fs.readFile("random.txt", "utf8", function(err, data){
-        if (err){
+    fs.readFile("random.txt", "utf8", function (err, data) {
+        if (err) {
             return console.log(err);
         }
 
         var paramSong = data;
 
-       var parsedData = paramSong.split(",");
+        var parsedData = paramSong.split(",");
 
-       command2 = parsedData[0];
-       content2 = parsedData[1];
+        command2 = parsedData[0];
+        content2 = parsedData[1];
 
-       command = command2;
-       content = content2
+        command = command2;
+        content = content2
 
-       spotifyThis();
+        spotifyThis();
 
     })
 
